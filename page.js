c5a24044e3941194d84971470e67f8f384e56b21
Daniel <oladepodaniel000@gmail.com>
2024-06-17 00:16:22 +0100

    added js file

---
+++ page.js
@@ -0,0 +1,7 @@
+// This is where you will add your web3 functionalities using libraries like Web3.js
+// For example, you could connect to a user's wallet here
+
+// Replace this with your actual functionality
+document.querySelector('.button').addEventListener('click', () => {
+  alert('Get Started button clicked!');
+});
